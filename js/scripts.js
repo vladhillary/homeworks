2097c56e6371d4df3ce93c9bbfbf123b75109cd4
Vlad <vladhillary@gmail.com>
2020-12-16 18:10:08 +0300

    hw_10_problems

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,49 @@ let Autopark = function () {
          this.calc() : alert('ну, как хотите');
     }
     this.calc = function(){
-        this.price = +prompt('Сколько стоит автомобиль(евро)?');
-        this.credit = confirm('Хотите авто взять в кредит?') ? this.time=+prompt('на сколько лет?') : document.write('Вы нищий, сэр!');
-        this.const = 0.25;
-        if (this.time !='') {this.sum = this.price + (this.price*this.const*this.time);}
-        this.result();
+        this.price = +prompt('Сколько стоит автомобиль(евро)?') || 10000;
+        this.credit = confirm('Хотите авто взять в кредит?');
+        if (this.credit == true) {
+            this.time=+prompt('на сколько лет?');
+        } else {
+            return document.write('Вы нищий, сэр!');
+        }
+         kom = 0.25;
+        if (this.time !='') {
+            this.sum = this.price + (this.price*kom*this.time);
+            this.result();
+        } else { 
+            document.write('Заного войдите в салон');
+        }
+        
     }
     this.result = function(){
         document.write(`Вы сделали заказ на приобритение в кредит автомобиля стиомостью в ${this.price} тысяч евро, сроком на ${this.time} лет, итого переплаты ${this.sum}`);
     }
 }
 let autoSalon = new Autopark();
-autoSalon.get();
+// autoSalon.get();
+
+let BuytFromHil = function(){
+    Autopark.apply(this);
+    let parentCalc = this.calc;
+    let sthis=this;
+    this.calc = function(){
+        parentCalc.call(this);
+        let kom2 = 0.15;
+        if (this.time !='') {
+            this.setsum();
+            this.sum = this.price + (this.price*kom2*this.time);
+        } else { 
+            document.write('Вы не выбрали время');
+            this.time=+prompt('на сколько лет?');
+        }
+        this.setsum = function(){
+            document.write(`А у нас вы заказали авто стоимостью в ${this.price}, сроком на ${this.time} лет, итого переплаты ${this.sum}`)
+        }
+    }; 
+}
+let buytFromIgor = new BuytFromHil;
+// console.log(buytFromIgor);
+
+buytFromIgor.get();
